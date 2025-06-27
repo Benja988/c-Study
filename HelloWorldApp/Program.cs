9da22bbc4726925178093e63b2d5f3e439b078de
Benjamin Okumu <okumub85@gmail.com>
2025-06-27 17:21:57 +0300

    Lesson 1

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Learning C# is fun!");
+Console.WriteLine("Let's Build awesome Apps!");
+
+
+// int age = 25;
+// string name = "Benja";
+// bool isLearning = true;
+
+Console.Write("What is Your Name?");
+string? inputName = Console.ReadLine();
+string name = inputName ?? string.Empty;
+
+Console.Write("How old are you?");
+int age = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Hello {name}, you are {age} years old.");
---
+++ TestApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Mr Benjmin!");
+Console.WriteLine("Coding is Fun");
+Console.WriteLine("Learning C# is Fun!");
+
+Console.Write("What is Your Dream Job");
+string? dreamJob = Console.ReadLine();
+Console.WriteLine($"Your Dream Job is {dreamJob}");
